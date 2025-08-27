fe82c095be12d9a9a1e3a17a3aefc583a6fb46cb
thorfdivine <saikykusuonopsiman@gmail.com>
2025-08-26 22:56:33 -0300

    realizei todas as mudanças necessarias, completando as classes e adicionando alguns detalhes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
+
+Nokia nokia = new Nokia("11 98739-9087", "A4", "asj234", 1024);
+Console.WriteLine(nokia);
+nokia.ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
